03320ae86d0cc9ad32c8497bb2417a6f1b8b590c
Henrik Hofmeister <henrik@newdawn.dk>
2023-06-17 21:45:57 +0200

    feat: Change to use typescript (#1)

---
+++ index.ts
@@ -0,0 +1,101 @@
+type RequestArgumentTransport = 'path' | 'header' | 'body' | 'query';
+type RequestMethod =
+    | 'GET'
+    | 'POST'
+    | 'DELETE'
+    | 'PATCH'
+    | 'PUT'
+    | 'OPTIONS'
+    | 'HEAD'
+    | 'TRACE'
+    | 'CONNECT'
+    | 'LINK'
+    | 'UNLINK'
+    | 'COPY'
+    | 'PURGE'
+    | 'LOCK'
+    | 'UNLOCK'
+    | 'PROPFIND'
+    | 'VIEW';
+export interface RequestArgument {
+    name: string;
+    value: any;
+    transport: RequestArgumentTransport;
+}
+
+export class RestClient {
+    private readonly _baseUrl: string;
+
+    /**
+     * Initialise rest client
+     *
+     * @param {string} baseUrl
+     */
+    constructor(baseUrl: string) {
+        if (!baseUrl) {
+            baseUrl = '/';
+        }
+
+        if (!baseUrl.endsWith('/')) {
+            baseUrl += '/';
+        }
+
+        this._baseUrl = baseUrl;
+    }
+
+    /**
+     *
+     * @param {string} method
+     * @param {string} path
+     * @param {RequestArgument[]} requestArguments
+     * @return {Promise<Object>}
+     */
+    async execute(method: RequestMethod, path: string, requestArguments: RequestArgument[] = []) {
+        while (path.startsWith('/')) {
+            path = path.substring(1);
+        }
+
+        let url = this._baseUrl + path;
+
+        const query: string[] = [];
+        const headers: { [key: string]: string } = {
+            accept: 'application/json',
+        };
+        const opts: RequestInit = {
+            method,
+            headers,
+        };
+
+        requestArguments.forEach((requestArgument) => {
+            switch (requestArgument.transport.toLowerCase()) {
+                case 'path':
+                    url = url.replaceAll('{' + requestArgument.name + '}', requestArgument.value);
+                    break;
+                case 'header':
+                    headers[requestArgument.name] = requestArgument.value;
+                    break;
+                case 'body':
+                    if (!headers['content-type']) {
+                        headers['content-type'] = 'application/json';
+                    }
+                    opts.body = JSON.stringify(requestArgument.value);
+                    break;
+                case 'query':
+                    query.push(
+                        encodeURIComponent(requestArgument.name) + '=' + encodeURIComponent(requestArgument.value)
+                    );
+                    break;
+                default:
+                    throw new Error('Unknown argument transport: ' + requestArgument.transport);
+            }
+        });
+
+        if (query.length > 0) {
+            url += '?' + query.join('&');
+        }
+
+        const result = await fetch(url, opts);
+
+        return result.json();
+    }
+}
